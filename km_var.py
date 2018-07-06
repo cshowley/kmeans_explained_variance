92528ba6b2802bafd56068c47f2bdafab1a4fe98
Charles Showley <12364081+cshowley@users.noreply.github.com>
2018-07-06 16:03:15 -0700

    Update km_var.py

--- km_var.py
+++ km_var.py
@@ -6,14 +6,25 @@ import pandas as pd
 
 
 def kmeans_explained_variance(k_min, k_max, data):
+	# Data is an NxM matrix consisting of N observations and M features
 	assert k_max > k_min
+	
+	# Calculate centroids for each model
 	k_means = range(k_min, k_max+1)
 	k_means_var = [KMeans(n_clusters=k).fit(data) for k in k_means]
 	centroids = [km.cluster_centers_ for km in k_means_var]
+	
+	# Assign observations in data to closest centroid
 	k_euclid = [cdist(df, cent, 'euclidean') for cent in centroids]
 	dist = [np.min(ke, axis=1) for ke in k_euclid]
+	
+	# Within-cluster sum of squares
 	wcss = [np.sum(d**2) for d in dist]
+	
+	# Total sum of squares
 	tss = np.sum(pdist(data)**2) / data.shape[0]
+	
+	# Between-cluster sum of squares
 	bcss = tss - wcss
 
 	return (k_means, bcss / tss)
